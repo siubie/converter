acf4244ba7bf085da2617d96d18fd369dcc2c572
Putra Prima Arhandi <putraprima@gmail.com>
2021-02-20 15:17:41 +0700

    Start cutting widgets

--- lib/widget/input.dart
+++ lib/widget/input.dart
@@ -4,13 +4,17 @@ import 'package:flutter/services.dart';
 class Input extends StatelessWidget {
   const Input({
     Key key,
+    @required this.etInput,
   }) : super(key: key);
+
+  final TextEditingController etInput;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       decoration: InputDecoration(hintText: "Masukkan Suhu Dalam Celcius"),
       inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+      controller: etInput,
       keyboardType: TextInputType.number,
     );
   }
